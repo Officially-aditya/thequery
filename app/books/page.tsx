38af6ba347001f22612db23dc638cbc3f612ea70
Aditya Yadav <93206009+Officially-aditya@users.noreply.github.com>
2026-09-09 22:17:23 +0530

    perf: make books index on-demand static

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
   }),
 };
 
-export const revalidate = 300;
+export const revalidate = false;
 
 export default async function BooksPage() {
   const books = await getAllBooks();
